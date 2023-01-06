5bc02866f16e59b1dcfd699560ebd17968d0bf70
Claire LESAFFRE <clesaffr@e1r7p16.clusters.42paris.fr>
2023-01-06 21:44:53 +0100

    launcher

--- philosophers.c
+++ philosophers.c
@@ -6,7 +6,7 @@
 /*   By: clesaffr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/06 19:53:38 by clesaffr          #+#    #+#             */
-/*   Updated: 2023/01/06 20:27:19 by clesaffr         ###   ########.fr       */
+/*   Updated: 2023/01/06 21:44:27 by clesaffr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,21 @@ int	put_error(char *str)
 	return (0);
 }
 
-int	parsing_rules(char **av, &rules)
+void	init_philos(t_philorules *rules)
+{
+	int	i;
+
+	i = 0;
+	while (i < rules->nbr_philos)
+	{
+		rules->philos[i].id = i;
+		rules->philos[i].ate = 0;
+		rules->philos[i].timestamp_ate = 0;
+		rules->philos[i].rules = rules;
+	}
+}
+
+int	parsing_rules(char **av, t_philorules *rules)
 {
 	rules->nbr_philos = ft_atoi(av[1]);
 	rules->t_die = ft_atoi(av[2]);
@@ -49,14 +63,48 @@ int	parsing_rules(char **av, &rules)
 	rules->t_sleep = ft_atoi(av[4]);
 	if (rules->nbr_philos > 250 || rules->nbr_philos < 0 || rules->t_die < 0 ||
 			rules->t_eat < 0 || rules->t_sleep < 0)
-			return (1);
+			return (-1);
 	if (av[5])
 	{
 		rules->nbr_eats = ft_atoi(av[5]);
 		if (rules->nbr_eats < 0)
-			return (1);
+			return (-1);
 	}
-	return (3);
+	init_philos(rules);
+	return (1);
+}
+
+void	*death_checker(void *void_philo)
+{
+}
+
+int	launch_thread(void *void_philo)
+{
+	t_philo 		*philo;
+	t_philorules	rules;
+
+	philo = (t_philo *)void_philo;
+	rules = philo->rules;
+	get_time_of_day(&philo->timestamp_ate, NULL);
+	pthread_create(&(philo->death_check), NULL, death_checker, void_philo);
+}
+
+int	launch(t_philorules *rules)
+{
+	int	i;
+	t_philo	*philos;
+
+	philos = rules->philos;
+	i = 0;
+	while (i < rules->nbr_philos)
+	{
+		philos[i].proc_id = fork();
+		if (philos[i].proc_id < 0)
+			return (-1);
+		if (philos[i].proc_id == 0)
+			launch_thread(&philos[i]);
+		i++;
+	}
 }
 
 int main(int ac, char **av)
@@ -65,6 +113,10 @@ int main(int ac, char **av)
 
 	if (ac != 5 || ac != 6)
 		return (put_error("Wrong number of arguments."));
-	parsing_rules(av, &rules);
+	if (parsing_rules(av, &rules) < 0)
+		return (put_error("Bad arguments : ./philo [nbr < 250]
+			[time_to_die] [time_to_eat] [time_to_sleep] (nb meals)"));
+	if (launch(&rules) < 0)
+		return (put_error("Error creating threads."));
 	return (0);
 }
--- philosophers.h
+++ philosophers.h
@@ -6,7 +6,7 @@
 /*   By: clesaffr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/06 17:19:23 by clesaffr          #+#    #+#             */
-/*   Updated: 2023/01/06 19:57:16 by clesaffr         ###   ########.fr       */
+/*   Updated: 2023/01/06 21:44:33 by clesaffr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,40 @@
 
 # include <stdio.h>
 # include <pthread.h>
+# include <sys/time.h>
+# include <sys/types.h>
+# include <unistd.h>
 
-typedef struct s_philorules
+struct s_philorules;
+
+typedef struct	s_timestamp
 {
-	int	nbr_philos;
-	int	t_die;
-	int	t_eat;
-	int	t_sleep;
-	int	nbr_eats;
+	time_t      tv_sec;
+	suseconds_t tv_usec;
+}				t_timestamp;
+
+typedef struct	s_philo
+{
+	int					id;
+	int					ate;
+	int					left_fork;
+	int					right_fork;
+	t_timestamp			timestamp_ate;
+	pid_t				proc_id;
+	pthread_t			death_check;
+	struct s_philorules	*rules;
+}				t_philo;
+
+typedef struct	s_philorules
+{
+	int			nbr_philos;
+	int			t_die;
+	int			t_eat;
+	int			t_sleep;
+	int			nbr_eats;
+	int			death;
+	t_timestamp	first_timestamp;
+	t_philo		philos[250];
 }				t_philorules;
 
 #endif
